8ceb7ce02bf4925dbda2547639952ff936b48bb5
Ashley Wu <me@ashleywu.net>
2024-04-17 22:54:19 -0700

    Initial commit

---
+++ src/components/Section1.tsx
@@ -0,0 +1,15 @@
+import '../styles/section1.scss';
+export default function Section1(): JSX.Element {
+  return (
+    // You will be coding here!
+    <div id="section1">
+      <p>about me</p>
+      <h1>hi! my name is ashley wu</h1>
+      <ul>
+        <li>i've had every hair color of the rainbow</li>
+        <li>i have a pet guinea pig</li>
+        <li>my favorite fruit is mango or lychee</li>
+      </ul>
+    </div>
+  );
+}
